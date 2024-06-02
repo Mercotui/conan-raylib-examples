024109272cf20f3167398886cbc86eacee03c21f
Menno van der Graaf <mennovandergraaf@hotmail.com>
2024-06-02 13:21:18 +0200

    Add custom shell for emscripten

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class RaylibWasm(ConanFile):
     def requirements(self):
         # Custom conan recipe, pending upstreaming:
         # https://github.com/Mercotui/conan-center-index/commit/ca075135a372741ca702c82326e2c927fa12c463
-        self.requires("raylib/5.0#cca0144de7cffb1c707231a3cf0dc3c9")
+        self.requires("raylib/5.0#3ba2ff34f19ef878e919de87156da5a1")
 
     def layout(self):
         cmake_layout(self)
